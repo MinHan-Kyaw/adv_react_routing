8b4af2b45f56cb321062405897900c49708b136d
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-07-03 21:09:37 +0700

    Submitting Data Programmatically

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -10,12 +10,14 @@ export default NewEventPage;
 
 export async function action({ request, params }) {
   const data = await request.formData();
+
   const eventData = {
     title: data.get("title"),
     image: data.get("image"),
     date: data.get("date"),
     description: data.get("description"),
   };
+  
   const response = await fetch("http://localhost:8080/events", {
     method: "POST",
     headers: {
