24a63ee01a635af970581bb8dca0b8e60a967b44
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-07-03 21:56:30 +0700

    Reusing Actions via Request Methods

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -3,6 +3,8 @@ import {
   useActionData,
   useNavigate,
   useNavigation,
+  json,
+  redirect,
 } from "react-router-dom";
 
 import classes from "./EventForm.module.css";
@@ -19,7 +21,7 @@ function EventForm({ method, event }) {
   }
 
   return (
-    <Form method="post" className={classes.form}>
+    <Form method={method} className={classes.form}>
       {data && data.errors && (
         <ul>
           {Object.values(data.errors).map((err) => (
@@ -33,7 +35,7 @@ function EventForm({ method, event }) {
           id="title"
           type="text"
           name="title"
-          // required
+          required
           defaultValue={event ? event.title : ""}
         />
       </p>
@@ -43,7 +45,7 @@ function EventForm({ method, event }) {
           id="image"
           type="url"
           name="image"
-          // required
+          required
           defaultValue={event ? event.image : ""}
         />
       </p>
@@ -53,7 +55,7 @@ function EventForm({ method, event }) {
           id="date"
           type="date"
           name="date"
-          // required
+          required
           defaultValue={event ? event.date : ""}
         />
       </p>
@@ -63,7 +65,7 @@ function EventForm({ method, event }) {
           id="description"
           name="description"
           rows="5"
-          // required
+          required
           defaultValue={event ? event.description : ""}
         />
       </p>
@@ -80,3 +82,37 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+export async function action({ request, params }) {
+  const method = request.method;
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  let url = "http://localhost:8080/events";
+
+  if (method === "PATCH") {
+    const eventId = params.eventId;
+    url = "http://localhost:8080/events/" + eventId;
+  }
+
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+  if (response.status === 422) {
+    return response;
+  }
+  if (!response.ok) {
+    throw json({ message: response }, { status: 500 });
+  }
+  return redirect("/events");
+}
