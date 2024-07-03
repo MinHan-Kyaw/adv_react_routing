f84573f987f706951d261d1fff2e0f9979b66a66
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-07-03 20:48:09 +0700

    Working with action() Functions

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -13,7 +13,6 @@ function EventsPage() {
 
 export async function loader() {
   const response = await fetch("http://localhost:8080/events");
-
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events." };
     // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
