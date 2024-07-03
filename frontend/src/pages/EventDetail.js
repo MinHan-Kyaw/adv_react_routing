73332c8ec2d0f1e8612356b44b3b42e49fcaaa83
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-07-03 17:22:32 +0700

    Dynamic Routes & loader()s

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,13 +1,20 @@
-import { useParams } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
+
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const params = useParams();
-  return (
-    <>
-      <h1>EventDetailPage</h1>
-      <p>Event ID: {params.eventId}</p>
-    </>
-  );
+  const data = useLoaderData();
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw json({ message: "Could not fetch event." }, { status: 500 });
+  } else {
+    return response;
+  }
+}
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -12,7 +12,7 @@ function EventsPage() {
 }
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/eventsafad");
+  const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events." };
